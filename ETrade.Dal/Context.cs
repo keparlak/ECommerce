776f933d71f81a52869e2b3b235b9acbf86d680f
Canberk Coşkun <canberkcoskun57@gmail.com>
2023-04-27 12:32:16 +0300

    E-Trade Start

---
+++ ETrade.Dal/Context.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETrade.Dal
+{
+	public class Context
+	{
+		//public Context(DbContextOptions<Context> op) : base(op)
+		//{
+
+		//}
+		//// DbSets
+		//public DbSet<Products> Products { get; set; }
+		//public DbSet<Categories> Categories { get; set; }
+	}
+}
---
+++ ETrade.UOW/Uow.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETrade.UOW
+{
+	public class Uow : IUow
+	{
+		public Uow() { }
+	}
+}
